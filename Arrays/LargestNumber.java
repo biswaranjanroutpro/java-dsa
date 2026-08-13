e0562b78b3806a4d01ae3e93414959b6fd4c0f40
Biswa Ranjan Rout <biswaranjanrout.pro@gmail.com>
2026-08-13 12:24:09 +0530

    feat(arrays) : add largest number

---
+++ Arrays/LargestNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class LargestNumber {
+    public static void main(String[] args) {
+        int numbers[] = {1, 2, 6, 3, 5};
+        System.out.println("Largest number is: " + getLargest(numbers));
+        
+    }
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] > largest) {
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+}
