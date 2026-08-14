c47a2555b175782ae618aeaf81817629055a67f3
Biswa Ranjan Rout <biswaranjanrout.pro@gmail.com>
2026-08-14 15:43:45 +0530

    feat(arrays) : add binary search

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+    public static int binarySearch(int numbers[], int key) {
+        int start = 0, end = numbers.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if(numbers[mid] == key) {
+                return mid;
+            } else if(numbers[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1; // Key not found
+    }
+
+    public static void main(String[] args) {
+        // for a binary search the sorted array is must imp
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int key = 10;
+        int result = binarySearch(numbers, key);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
