b25c29be2e559be6c5caf3916d36b0901e71a642
Biswa Ranjan Rout <biswaranjanrout.pro@gmail.com>
2026-08-14 16:04:09 +0530

    feat(arrays) : add reverse an array

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,25 @@
+public class ReverseArray {
+    public static void reverse(int numbers[]) {
+        int start = 0, end = numbers.length - 1; 
+        while(start < end) {
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8,10};
+
+        // reverse array
+        reverse(numbers);
+        
+        // print array
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+}
