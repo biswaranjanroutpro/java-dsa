226e7bbe8b22692b35f50c62513c61568906e3ac
Biswa Ranjan Rout <biswaranjanrout.pro@gmail.com>
2026-08-16 21:10:51 +0530

    feat(arrays) : add prefix sum

--- Arrays/MaximumSubarraysSum.java
+++ Arrays/MaximumSubarraysSum.java
@@ -21,8 +21,32 @@ public class MaximumSubarrraysSum {
         }
         System.out.println(ms);
     }
+
+    // Prefix sum 
+    public static void prefixSum(int[] arr) {
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
+
+        prefix[0] = arr[0];
+        // calculate prefix array
+        for(int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i; j < arr.length; j++) {
+                cs = i == 0 ? prefix[j] : prefix[j] - prefix[j-1];
+                if(ms < cs) {
+                    ms = cs;
+                }
+            }
+        }
+        System.out.println(ms);
+    }
     public static void main(String[] args) {
         int numbers[] = {2, 4, 6, 8, 10};
-        maxSubarraySum(numbers);
+        // maxSubarraySum(numbers);
+        prefixSum(numbers);
     }
 }
